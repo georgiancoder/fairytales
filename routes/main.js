61d294291af630c523512a89b1802ec7f8702ed5
Beka Tsalani <beqacalani@gmail.com>
2018-06-10 18:24:36 +0400

    added tale page

--- routes/main.js
+++ routes/main.js
@@ -5,9 +5,16 @@ const Categorie = require('../controllers/categorieController');
 router.get('/',(req,res)=>{
 	let opt = {
 		page: 'home'
-	}
+	};
 	res.render('template/index',opt);
 
+});
+
+router.get('/tale/:id',(req,res)=>{
+	let opt = {
+		page: 'tale'
+	};
+	res.render('template/tale',opt);
 });
 
 
