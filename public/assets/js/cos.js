2a95c99eaec0bd45d4113dd71687c5f965580b20
vaxosv <vaxo.sv@gmail.com>
2018-06-15 23:37:21 +0400

    main slider changghe

--- public/assets/js/cos.js
+++ public/assets/js/cos.js
@@ -1,12 +1,43 @@
 var menuli = $("#cat");
 var slider = $("#sli");
 var bull = false;
-slider.hide(0);
 menuli.on("click", function () {
     if (bull) {
         slider.slideUp();
     } else {
         slider.slideDown();
+        slide();
     }
     bull = !bull
 })
+
+
+
+function slide() {
+    
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 8,
+    spaceBetween: 5,
+    loop: true,
+    breakpoints: {
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 5,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 5,
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 5,
+        },
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 5,
+        }
+    }
+});
+
+}
