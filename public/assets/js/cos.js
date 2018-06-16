5d5c553d536377fe4bd34eaa4e672202e351f629
vaxosv <vaxo.sv@gmail.com>
2018-06-16 15:08:22 +0400

    responcive menu

--- public/assets/js/cos.js
+++ public/assets/js/cos.js
@@ -14,30 +14,42 @@ menuli.on("click", function () {
 
 
 function slide() {
-    
 
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 8,
-    spaceBetween: 5,
-    loop: true,
-    breakpoints: {
-        1024: {
-            slidesPerView: 4,
-            spaceBetween: 5,
-        },
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 5,
-        },
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 5,
-        },
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 5,
+
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 8,
+        spaceBetween: 5,
+        loop: true,
+        breakpoints: {
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 5,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 5,
+            },
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 5,
+            }
         }
-    }
-});
+    });
 
+}
+
+
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
